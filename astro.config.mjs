93d299f1af8fadfd662ff14d3e196a3c5b4fc617
Light <farhan29110511@gmail.com>
2026-02-04 01:33:07 +0530

    fix: backtick quote render & LaTex formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 import react from "@astrojs/react";
 
@@ -11,6 +13,8 @@ export default defineConfig({
   vite: { plugins: [tailwindcss()] },
   integrations: [react(), mdx()],
   markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
     shikiConfig: {
       themes: {
         light: "github-dark",
